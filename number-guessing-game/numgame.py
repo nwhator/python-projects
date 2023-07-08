9004f9008fa5e5e52114f9641ca14ac1b24dba91
nwhator <nwhator@gmail.com>
2023-07-08 07:41:37 -0700

    Added 3 levels to the game

--- number-guessing-game/numgame.py
+++ number-guessing-game/numgame.py
@@ -3,8 +3,13 @@ import random
 
 
 # Function to generate a random number between 1 and 10
-def generate_random_number():
-    return random.randint(1, 10)
+def generate_random_number(level):
+    if level == 1:
+        return random.randint(1, 10)
+    elif level == 2:
+        return random.randint(1, 25)
+    elif level == 3:
+        return random.randint(1, 50)
 
 
 # Function to get user's name
@@ -17,16 +22,25 @@ def get_user_name():
 def get_user_guess():
     while True:
         try:
-            guess = int(input("Guess the number (between 1 and 10): "))
+            guess = int(input("Guess the number: "))
             return guess
         except ValueError:
             print("Invalid input! Please enter a valid number.")
 
 
 # Function to validate user's guess
-def validate_guess(guess, target):
-    if guess < 1 or guess > 10:
-        print("Invalid guess! The number should be between 1 and 10.")
+def validate_guess(guess, target, level):
+    if level == 1:
+        valid_range = (1, 10)
+    elif level == 2:
+        valid_range = (1, 25)
+    elif level == 3:
+        valid_range = (1, 50)
+    else:
+        valid_range = (1, target)
+
+    if guess < valid_range[0] or guess > valid_range[1]:
+        print(f"Invalid guess! The number should be between {valid_range[0]} and {valid_range[1]}.")
         return False
     return True
 
@@ -40,18 +54,21 @@ def provide_hint(guess, target):
 
 
 # Function to play the number guessing game
-def play_game():
-    target_number = generate_random_number()
-    guess_limit = 3
+def play_game(level):
+    target_number = generate_random_number(level)
     guesses_taken = 0
+    guess_limit = get_guess_limit(level)
 
     name = get_user_name()
     print(f"Hello {name}! Let's play the Number Guessing Game.")
+    print(f"\nLevel {level}")
+    print("---------")
+    print(f"You have {guess_limit} guesses to find the number.")
 
     while guesses_taken < guess_limit:
         guess = get_user_guess()
 
-        if not validate_guess(guess, target_number):
+        if not validate_guess(guess, target_number, level):
             continue
 
         guesses_taken += 1
@@ -66,16 +83,39 @@ def play_game():
     return True
 
 
+# Function to get the guess limit for each level
+def get_guess_limit(level):
+    if level == 1:
+        return 3
+    elif level == 2:
+        return 5
+    elif level == 3:
+        return 10
+
 # Main program
 play_again = True
 score = 0
 
-while play_again:
-    game_result = play_game()
-    if game_result:
-        score += 1
+print("Welcome to the Number Guessing Game!")
+print("------------------------------------")
 
-    print(f"Your current score: {score}")
+while play_again:
+    level = 1
+    while level <= 3:
+        guess_limit = get_guess_limit(level)
+        print(f"\nLevel {level}")
+        print(f"You have {guess_limit} guesses available.")
+        game_result = play_game(level)
+        if game_result:
+            score += 1
+            level += 1
+        else:
+            choice = input("Do you want to restart the level? (yes/no): ")
+            if choice.lower() != "yes":
+                break
+
+
+    print(f"\nGame Over! Your final score: {score}")
 
     while True:
         choice = input("Do you want to play again? (yes/no): ")
@@ -84,5 +124,6 @@ while play_again:
         else:
             print("Invalid input! Please enter 'yes' or 'no'.")
     play_again = choice.lower() == "yes"
+    score = 0
 
-print(f"Thanks for playing! Your final score: {score}")
+print(f"Thanks for playing!")
